9decd65d73c41602606715802464044cc445ebf9
frncs99 <francisresugenio09@gmail.com>
2024-03-18 14:41:33 +0800

    store backend

--- app/Actions/BaseService.php
+++ app/Actions/BaseService.php
@@ -30,4 +30,29 @@ class BaseService
     {
         return $this->model->get();
     }
+
+    /**
+     * @return $this|bool
+     */
+    public function store($request, string $id = null)
+    {
+        if ($id !== null) {
+            $this->model = $this->model->find($id);
+            if (! $this->model) {
+                return false;
+            }
+        }
+
+        $fieldColumns = $this->model->getFillable();
+
+        foreach ($fieldColumns as $column) {
+            if ($request->has($column)) {
+                $this->model[$column] = $request[$column];
+            }
+        }
+
+        $this->model->save();
+
+        return $this->model;
+    }
 }
--- app/Http/Controllers/Web/CrudTestController.php
+++ app/Http/Controllers/Web/CrudTestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Actions\CrudTest\CrudTestService;
 use App\Http\Controllers\Controller;
-use App\Models\CrudTest;
+use App\Http\Requests\CrudTest\StoreRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -30,5 +30,28 @@ class CrudTestController extends Controller
         ]);
     }
 
-    
+    /**
+     * Display the form for creating a new resource.
+     *
+     * @return \Inertia\Inertia
+     */
+    public function create()
+    {
+        return Inertia::render('CrudTest/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\CrudTest\StoreRequest  $request
+     * @return void
+     */
+    public function store(StoreRequest $request)
+    {
+        $this->modelService->store($request);
+
+        session()->flash('message', __('messages.crud_test.success.store'));
+
+        return;
+    }
 }
