791405088c26b9b25f464bda851b5120a25c4268
frncs99 <francisresugenio09@gmail.com>
2024-03-18 14:40:50 +0800

    localization

---
+++ lang/en/messages.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are for various custom
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'crud_test' => [
+        'success' => [
+            'store' => 'Successfully created item.',
+            'update' => 'Successfully updated item.',
+            'destroy' => 'Successfully deleted item.',
+        ],
+    ],
+
+];
